872e783e18cd919a3fe99e2781d7f41223a5643d
김태우 <taewoo080401@gmail.com>
2026-01-16 13:49:59 +0900

    feat: create themes, colors, typography, shapes packages

---
+++ packages/colors/src/colors.ts
@@ -0,0 +1,32 @@
+import { Colors } from "./types";
+
+export const colors: Colors = {
+  greyScale: {
+    0: "var(--bds-color-greyscale-0)",
+    10: "var(--bds-color-greyscale-10)",
+    20: "var(--bds-color-greyscale-20)",
+    30: "var(--bds-color-greyscale-30)",
+    40: "var(--bds-color-greyscale-40)",
+    50: "var(--bds-color-greyscale-50)",
+    60: "var(--bds-color-greyscale-60)",
+    70: "var(--bds-color-greyscale-70)",
+    80: "var(--bds-color-greyscale-80)",
+    90: "var(--bds-color-greyscale-90)",
+  },
+  red: {
+    light: "var(--bds-color-red-light)",
+    dark: "var(--bds-color-red-dark)",
+  },
+  blue: {
+    light: "var(--bds-color-blue-light)",
+    dark: "var(--bds-color-blue-dark)",
+  },
+  green: {
+    light: "var(--bds-color-green-light)",
+    dark: "var(--bds-color-green-dark)",
+  },
+  static: {
+    white: "var(--bds-color-static-white)",
+    black: "var(--bds-color-static-black)",
+  },
+};
---
+++ packages/colors/src/types.ts
@@ -0,0 +1,34 @@
+export interface Colors {
+  greyScale: {
+    0: string;
+    10: string;
+    20: string;
+    30: string;
+    40: string;
+    50: string;
+    60: string;
+    70: string;
+    80: string;
+    90: string;
+  };
+
+  red: {
+    light: string;
+    dark: string;
+  };
+
+  blue: {
+    light: string;
+    dark: string;
+  };
+
+  green: {
+    light: string;
+    dark: string;
+  };
+  
+  static: {
+    white: string;
+    black: string;
+  };
+}
